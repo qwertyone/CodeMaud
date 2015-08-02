032fb3853b29fa5755703267ce08cfbdcba8126e
qwertyone <qwertyone@users.noreply.github.com>
2015-08-03 00:05:06 +0200

    Create Update.R
    Initial File List

---
+++ Update.R
@@ -0,0 +1,10 @@
+##initiate packages
+library(rvest)
+library(XML)
+library("selectr")
+
+##begin update of URLs and files to download
+url<-"http://www.fda.gov/MedicalDevices/DeviceRegulationandGuidance/PostmarketRequirements/ReportingAdverseEvents/ucm127891.htm"
+listFiles <- html(url, encoding = "UTF-8")
+listFiles <- listFiles %>% html_nodes("table")
+
