3f68fabcf8635eca1b78e1063dc3026b0adae745
qwertyone <qwertyone@users.noreply.github.com>
2015-08-03 10:59:19 +0200

    Update Update.R

--- Update.R
+++ Update.R
@@ -5,6 +5,9 @@ library("selectr")
 
 ##begin update of URLs and files to download
 url<-"http://www.fda.gov/MedicalDevices/DeviceRegulationandGuidance/PostmarketRequirements/ReportingAdverseEvents/ucm127891.htm"
-listFiles <- html(url, encoding = "UTF-8")
-listFiles <- listFiles %>% html_nodes("table")
+listFiles <- getHTMLLinks(url, externalOnly = TRUE)
+listFiles <- listFiles[31:84]
 
+##begin download and extraction of zip files and save file as csv
+
+##clean data for organizational level dataframe
