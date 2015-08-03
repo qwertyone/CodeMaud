c232e6b03d98a02c0ab8f08f7da63bf70d2ad53c
qwertyone <qwertyone@users.noreply.github.com>
2015-08-03 11:18:44 +0200

    Update Update.R

--- Update.R
+++ Update.R
@@ -9,5 +9,17 @@ listFiles <- getHTMLLinks(url, externalOnly = TRUE)
 listFiles <- listFiles[31:84]
 
 ##begin download and extraction of zip files and save file as csv
+dir = "D:/MAUDE"
+setwd(dir)
+
+for file in listFiles:
+  temp = tempfile()
+  download.file(file,temp)
+  data <- read.table(unz(temp, file))
+  unlink(temp)
+  ##retrieve file name
+  write.csv(data, file, sep = "\t", row.names = FALSE, col.names = TRUE)
+
+
 
 ##clean data for organizational level dataframe
