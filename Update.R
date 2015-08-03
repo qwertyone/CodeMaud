2541406b230d1e26c578b2cd390cb220902a2cd3
qwertyone <qwertyone@users.noreply.github.com>
2015-08-03 17:57:33 +0200

    Update Update.R

--- Update.R
+++ Update.R
@@ -22,7 +22,7 @@ for (file in listFiles) {
   ##set directory for saving data prior to cleaning
   n <- paste("D:/MAUDE/RawData/",fileName,sep="")
   n <- paste(n,".csv",sep="")
-  write.csv(data, filename = n, sep = "\t", row.names = FALSE, col.names = TRUE)
+  write.csv(data, file = n, sep = "\t", row.names = FALSE, col.names = TRUE)
 }
 
 ##clean data for organizational level dataframe
